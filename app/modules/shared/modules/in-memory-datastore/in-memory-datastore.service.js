04f3562787efdd10242525dea922a6451fba0c02
Shripal Soni <shripalsoni04@gmail.com>
2016-04-08 11:45:17 +0530

    Added ability to parse url like project/:id and return data of project for that id in get method.

--- app/modules/shared/modules/in-memory-datastore/in-memory-datastore.service.js
+++ app/modules/shared/modules/in-memory-datastore/in-memory-datastore.service.js
@@ -4,7 +4,7 @@ define(function() {
         that.$q = $q;
         that._datastore = null;
         that.inMemoryDatastoreApiEndPoint = inMemoryDatastoreApiEndPoint;
-        
+
         that.datastoreReadyPromise = $q(function(resolve) {
             require([inMemoryDatastorePath], function(datastore) {
                 that._datastore = datastore;
@@ -14,14 +14,58 @@ define(function() {
     }
 
     InMemoryDatastoreService.prototype._loadDatastore = function() {
-        return this._datastore ? this.$q.when(this._datastore) : this.datastoreReadyPromise; 
+        return this._datastore ? this.$q.when(this._datastore) : this.datastoreReadyPromise;
+    }
+
+    InMemoryDatastoreService.prototype._getEntityName = function(url) {
+        return url.replace(this.inMemoryDatastoreApiEndPoint, '');
+    }
+
+    InMemoryDatastoreService.prototype._getLocation = function(href) {
+        var l = document.createElement('a');
+        l.href = href;
+        return l;
+    };
+    InMemoryDatastoreService.prototype._parseUrl = function(url) {
+        var location = this._getLocation(url);
+        var pathname = location.pathname.substring(1);
+        var pathParts = pathname.split('/');
+        var base = pathParts[0];
+        var entityName = pathParts[1];
+        var id = pathParts[2];
+        return {
+            base: base,
+            entityName: entityName,
+            id: id
+        };
     }
     
-    InMemoryDatastoreService.prototype._getEntityName = function(url){
-        return url.replace(this.inMemoryDatastoreApiEndPoint, '');
+    
+    InMemoryDatastoreService.prototype._getList = function(entityName) {
+        return this._loadDatastore().then(function(datastore) {
+            if (!datastore[entityName]) {
+                datastore[entityName] = [];
+            }
+            return this.$q.when(datastore[entityName]);
+        }.bind(this));
+    }
+    
+    InMemoryDatastoreService.prototype._getById = function(entityName, id) {
+        return this._getList(entityName).then(function(collection){
+            return _.find(collection, {id: +id});
+        }.bind(this));
     }
     
     InMemoryDatastoreService.prototype.get = function(url) {
+        var oUrlDetail = this._parseUrl(url);
+        if(oUrlDetail.id){
+            return this._getById(oUrlDetail.entityName, oUrlDetail.id);
+        }else{
+            return this._getList(oUrlDetail.entityName);
+        }
+    }
+
+    InMemoryDatastoreService.prototype.getById = function() {
         var entityName = this._getEntityName(url);
         return this._loadDatastore().then(function(datastore) {
             if (!datastore[entityName]) {
