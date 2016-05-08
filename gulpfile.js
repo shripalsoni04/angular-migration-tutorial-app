2d44d5b2a02310c4832f5eac43a17d1826686b68
Shripal Soni <shripalsoni04@gmail.com>
2016-05-08 19:52:02 +0530

    code formatting.

--- gulpfile.js
+++ gulpfile.js
@@ -4,28 +4,28 @@ var browserSync = require('browser-sync');
 var exec = require('child_process').exec;
 var reload = browserSync.reload;
 
-gulp.task('sass', function() {
+gulp.task('sass', function () {
   return sass('client/styles/scss/**/*.scss')
     .pipe(gulp.dest('client/styles/css'))
-    .pipe(reload({ stream:true }));
+    .pipe(reload({ stream: true }));
 });
 
-gulp.task('serve', ['sass'], function(cb){
-    // starting api server
-    exec('node server', function(err, stdout, stderr){
-      /*eslint no-console: "off" */
-      console.info(stdout);
-      console.error(stderr);
-      cb(err);
-    });
-    
-    // starting client server
-    browserSync({
+gulp.task('serve', ['sass'], function (cb) {
+  // starting api server
+  exec('node server', function (err, stdout, stderr) {
+    /*eslint no-console: "off" */
+    console.info(stdout);
+    console.error(stderr);
+    cb(err);
+  });
+
+  // starting client server
+  browserSync({
     server: {
       baseDir: 'client'
     }
   });
-  
+
   gulp.watch(['client/**/*.js', 'client/**/*.html', '!client/bower_components/**/*.*'], reload);
   gulp.watch('client/styles/scss/**/*.scss', ['sass']);
 });
