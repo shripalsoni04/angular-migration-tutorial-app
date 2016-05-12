5df3a7fbbf747822ee3b589efd56958999ead875
Shripal Soni <shripalsoni04@gmail.com>
2016-05-12 11:54:54 +0530

    minor refactoring of module import ordering for consistency.

--- client/modules/dashboard/dashboard.module.js
+++ client/modules/dashboard/dashboard.module.js
@@ -5,10 +5,10 @@
 define([
     'angular',
     'angular-ui-router',
+    'modules/project/project.module',
     './dashboard.routes',
-    './dashboard.controller',
-    'modules/project/project.module'
-], function(angular, uiRouterModule, dashboardRouteConfig, DashboardCtrl, projectModule) {
+    './dashboard.controller'
+], function(angular, uiRouterModule, projectModule, dashboardRouteConfig, DashboardCtrl) {
     'use strict';
 
     return angular.module('app.dashboard', ['ui.router', projectModule.name])
